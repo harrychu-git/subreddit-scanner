99f7bca64d4f086c32f8650616e5ab93e178a639
hchu007 <sir.harrychu@gmail.com>
2022-07-24 12:36:09 -0700

    Implemented send email functionality using gmail SMTP

--- scan.py
+++ scan.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
-import sys, getopt, requests, os, time
+import sys, getopt, requests, os, time, smtplib
 # Run `pip3 install python-dotenv`
 from dotenv import load_dotenv
+from email.message import EmailMessage
 
 def printSyntax():
-	print('usage: python3 scan.py -u <reddit_username> -p <reddit_password> -s <subreddit_name> -t <text>')
+	print('usage: python3 scan.py -u <reddit_username> -p <reddit_password> -s <subreddit_name> -t <text> -e <email>')
 
 def main(argv):
 	### VALIDATE ARGUMENTS ###
@@ -13,19 +14,20 @@ def main(argv):
 	password = ''
 	subreddit = ''
 	text = ''
-	if len(sys.argv) != 9:
-		print('Invalid number of arguments! Expected arguments: 9. Actual arguments: ', len(sys.argv))
+	email = ''
+	if len(sys.argv) != 11:
+		print('Invalid number of arguments! Expected arguments: 11. Actual arguments: ', len(sys.argv))
 		printSyntax()
 		sys.exit()
 	try:
-		opts, args = getopt.getopt(argv,"u:p:s:t:",["username=","password=","subreddit=","text="])
+		opts, args = getopt.getopt(argv,"u:p:s:t:e:",["username=","password=","subreddit=","text=","email="])
 	except getopt.GetoptError:
 		print("Unexpected argument.")
 		printSyntax()
 		sys.exit()
 	
 	for opt, arg in opts:
-		if opt != '-u' and opt != '-p' and opt != '-s' and opt != '-t':
+		if opt != '-u' and opt != '-p' and opt != '-s' and opt != '-t' and opt != '-e':
 			printSyntax()
 			sys.exit()
 		elif opt in ("-u", "--username"):
@@ -36,11 +38,10 @@ def main(argv):
 			subreddit = arg
 		elif opt in ("-t", "--text"):
 			text = arg
+		elif opt in ("-e", "--email"):
+			email = arg
 
 	url = 'https://oauth.reddit.com/r/' + subreddit + '/new.json?limit=20'
-	print('Username is: ', username)
-	print('URL is: ', url)
-	print('Text is: ', text)
 
 	### Set up reddit API authentication ###
 	load_dotenv()
@@ -62,10 +63,21 @@ def main(argv):
 
 		### JSON PARSER ###	
 		for post in res.json()['data']['children']:
-			print(post['data']['title'])
 			if text.lower() in post['data']['title'].lower():
+				### SEND EMAIL ###
 				isMatch = True;
-				print("Match found: ", text.lower(), " (Implement some notifier)")
+				msg = EmailMessage()
+				msg['Subject'] = "Match found for text: " + text.lower() + " in subreddit: " + subreddit
+				msg['From'] = "subredditscanner@gmail.com"
+				msg['To'] = email
+				msg.set_content(post['data']['url'])
+
+				smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+				smtp.login("subredditscanner@gmail.com", os.getenv("EMAIL_PASSWD")) 
+				smtp.send_message(msg)
+				smtp.close()
+				break
+
 		if not isMatch:
 			time.sleep(300)
 	### END LOOP ###
